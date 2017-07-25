eeadd03562233d7e863ea8c30b6fb32b16a515c2
erikvw <ew2789@gmail.com>
2017-07-25 12:24:51 +0200

    add populater

--- edc_reference/populater.py
+++ edc_reference/populater.py
@@ -1,5 +1,32 @@
+import arrow
+import sys
+
+from django.apps import apps as django_apps
+
+from .reference import ReferenceUpdater
+from .site import site_reference_configs
+
+
+class PopulaterAttributeError(Exception):
+    pass
+
 
 class Populater:
 
-    def __init__(self):
-        pass
+    def populate(self):
+        t = arrow.utcnow().to('Africa/Gaborone').strftime('%H:%M')
+        sys.stdout.write(
+            f'Populating reference model. Started: {t}\n')
+        for model in site_reference_configs.registry:
+            index = 0
+            sys.stdout.write(f'{model}           \r')
+            model_cls = django_apps.get_model(model)
+            qs = model_cls.objects.all()
+            total = qs.count()
+            for index, model_obj in enumerate(qs):
+                index += 1
+                sys.stdout.write(f'{model} {index} / {total} ...    \r')
+                ReferenceUpdater(model_obj=model_obj)
+            sys.stdout.write(f'{model} {index} / {total} . OK    \n')
+        t = arrow.utcnow().to('Africa/Gaborone').strftime('%H:%M')
+        sys.stdout.write(f'Done. Ended: {t}\n')
