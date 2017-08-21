b92a986b1a747964338e3144f8085b47370b44cc
erikvw <ew2789@gmail.com>
2017-08-21 14:34:28 +0200

    add home url, admin site

---
+++ edc_reference/admin_site.py
@@ -0,0 +1,11 @@
+from django.contrib.admin.sites import AdminSite
+
+
+class EdcReferenceAdminSite(AdminSite):
+    site_header = 'Edc Reference'
+    site_title = 'Edc Reference'
+    index_title = 'Edc Reference'
+    site_url = '/admininistration/'
+
+
+edc_reference_admin = EdcReferenceAdminSite(name='edc_reference_admin')
