9b73f1bdc76e86d842f18071ecaffe67ebdfbc86
erikvw <ew2789@gmail.com>
2017-09-03 20:24:58 +0200

    trivial

--- edc_reference/reference/reference_getter.py
+++ edc_reference/reference/reference_getter.py
@@ -2,7 +2,6 @@ from django.apps import apps as django_apps
 from django.core.exceptions import ObjectDoesNotExist
 
 from ..site import site_reference_configs
-from pprint import pprint
 
 
 class ReferenceObjectDoesNotExist(Exception):
