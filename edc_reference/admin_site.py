616a0514e52b8a4a009ca6b8853f54c74fa29828
erikvw <ew2789@gmail.com>
2017-11-09 13:21:24 +0200

    add navbars

--- edc_reference/admin_site.py
+++ edc_reference/admin_site.py
@@ -5,7 +5,7 @@ class EdcReferenceAdminSite(AdminSite):
     site_header = 'Edc Reference'
     site_title = 'Edc Reference'
     index_title = 'Edc Reference'
-    site_url = '/admininistration/'
+    site_url = '/administration/'
 
 
 edc_reference_admin = EdcReferenceAdminSite(name='edc_reference_admin')
